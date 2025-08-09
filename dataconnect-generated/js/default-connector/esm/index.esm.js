4ae488b55f239a3c5a4075e7911b82d22acdfe7a
Faded Skies <fadedskies@Fadeds-Air.attlocal.net>
2025-08-09 13:39:38 -0500

    Initialize Firebase project with all services

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'wholesale-95ceb-service',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'wholesale-95ceb-service',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
